b516651f9f78e1ee9f5a1670666093e3136aa954
Murali <muralikrishna1991@gmail.com>
2018-07-27 18:21:37 +0530

    Model Validations

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,7 @@
 class Question < ApplicationRecord
   belongs_to :survey
+
+  has_many :answers
+
+  validates_presence_of :title
 end
--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,2 +1,4 @@
 class Survey < ApplicationRecord
+	validates_presence_of :title
+	validates_uniqueness_of :title
 end
